af13458357f36d8cba6601a0fce953463f26c13b
Dmitry Savelev <dsavelev1703@yandex.ru>
2022-12-25 19:48:25 +0700

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://avatars.mds.yandex.net/i?id=bef656c24663bc119d6d85e56a19a69f9517eaf7-7546007-images-thumbs&n=13&exp=1" />
+    </header>
+  );
+};
+
+export default Header;
